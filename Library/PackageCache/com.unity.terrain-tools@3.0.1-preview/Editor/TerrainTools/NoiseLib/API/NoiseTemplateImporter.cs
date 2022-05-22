87b838204d2d8535852637c6e37fcbd97b9b55db
Elia Oliosi <elia.oliosi@elettrone.com>
2022-05-22 15:34:31 +0200

    Initial commit

--- Library/PackageCache/com.unity.terrain-tools@3.0.1-preview/Editor/TerrainTools/NoiseLib/API/NoiseTemplateImporter.cs
+++ Library/PackageCache/com.unity.terrain-tools@3.0.1-preview/Editor/TerrainTools/NoiseLib/API/NoiseTemplateImporter.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
-using UnityEditor.Experimental.AssetImporters;
+
 
 namespace UnityEditor.Experimental.TerrainAPI
 {
     /// <summary>
     /// Asset importer for noise template files. Used to detect when a noise template is imported and checks to see if the shaders need to be regenerated.
     /// </summary>
-    [ScriptedImporter(1, "noisehlsltemplate")]
-    public class NoiseTemplateImporter : ScriptedImporter
+    [UnityEditor.AssetImporters.ScriptedImporter(1, "noisehlsltemplate")]
+    public class NoiseTemplateImporter : UnityEditor.AssetImporters.ScriptedImporter
     {
         /// <summary>
         /// Function that is called when an asset with the ".noisehlsltemplate" extension is imported by the AssetDatabase
         /// </summary>
         /// <param name = "ctx"> The context for the imported asset </param>
-        public override void OnImportAsset(AssetImportContext ctx)
+        public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext ctx)
         {
             NoiseLib.GenerateHeaderFiles();
             NoiseLib.GenerateShaders();
