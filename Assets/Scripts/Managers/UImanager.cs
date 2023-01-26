341ca1a4a50693c6aee75da824677d652ce1217f
Elia Oliosi <elia.oliosi96@live.it>
2023-01-26 22:47:52 +0100

    hotfix ui resolution

--- Assets/Scripts/Managers/UImanager.cs
+++ Assets/Scripts/Managers/UImanager.cs
@@ -28,43 +28,46 @@ namespace Assets.Scripts.Managers.UI
     private void CreateSpellSlots()
     {
       //var playerSpellContainsers = PlayerEntity.Instance.GetComponents<PlayerSpellContainer>().ToList();
-      var playerSpellsAmount = 3;
+      //var playerSpellsAmount = 3;
 
-      var width = 96;
+      //var width = 96;
       var inputs = GUI.Find("Inputs");
-      var resource = Resources.Load<GameObject>($"UI/SpellSlot");
+      //var resource = Resources.Load<GameObject>($"UI/SpellSlot");
+      //
+      //var offsetX = -width * (playerSpellsAmount / 2);
 
-      var offsetX = -width * (playerSpellsAmount / 2);
+      var spellContainers = inputs.GetComponentsInChildren<UIspell>();
+      uiSpells.AddRange(spellContainers);
 
-      /*** Reanimate ***/
-      var reanimateSpellSlot = Instantiate(resource, inputs, true);
-      reanimateSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 0, 48);
-      var reanimateUI = reanimateSpellSlot.GetComponent<UIspell>();
-      reanimateUI.spellKind = SpellKind.Reanimate;
-      reanimateUI.spellName = "Reanimate";
-      reanimateUI.spellStance = PlayerStance.Combat;
-      reanimateUI.spellCost = 0;
-      uiSpells.Add(reanimateUI);
-
-      /*** Consume ***/
-      var consumeSpellSlot = Instantiate(resource, inputs, true);
-      consumeSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 1, 48);
-      var consumeUI = consumeSpellSlot.GetComponent<UIspell>();
-      consumeUI.spellKind = SpellKind.Consume;
-      consumeUI.spellName = "Consume";
-      consumeUI.spellStance = PlayerStance.Combat;
-      consumeUI.spellCost = -1;
-      uiSpells.Add(consumeUI);
-
-      /*** DarkHail ***/
-      var darkHailSpellSlot = Instantiate(resource, inputs, true);
-      darkHailSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 2, 48);
-      var darkHailUI = darkHailSpellSlot.GetComponent<UIspell>();
-      darkHailUI.spellKind = SpellKind.DarkHail;
-      darkHailUI.spellName = "DarkHail";
-      darkHailUI.spellStance = PlayerStance.Combat;
-      darkHailUI.spellCost = 1;
-      uiSpells.Add(darkHailUI);
+      ///*** Reanimate ***/
+      //var reanimateSpellSlot = Instantiate(resource, inputs, true);
+      //reanimateSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 0, 48);
+      //var reanimateUI = reanimateSpellSlot.GetComponent<UIspell>();
+      //reanimateUI.spellKind = SpellKind.Reanimate;
+      //reanimateUI.spellName = "Reanimate";
+      //reanimateUI.spellStance = PlayerStance.Combat;
+      //reanimateUI.spellCost = 0;
+      //uiSpells.Add(reanimateUI);
+      //
+      ///*** Consume ***/
+      //var consumeSpellSlot = Instantiate(resource, inputs, true);
+      //consumeSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 1, 48);
+      //var consumeUI = consumeSpellSlot.GetComponent<UIspell>();
+      //consumeUI.spellKind = SpellKind.Consume;
+      //consumeUI.spellName = "Consume";
+      //consumeUI.spellStance = PlayerStance.Combat;
+      //consumeUI.spellCost = -1;
+      //uiSpells.Add(consumeUI);
+      //
+      ///*** DarkHail ***/
+      //var darkHailSpellSlot = Instantiate(resource, inputs, true);
+      //darkHailSpellSlot.transform.localPosition = new Vector3(offsetX + 96 * 2, 48);
+      //var darkHailUI = darkHailSpellSlot.GetComponent<UIspell>();
+      //darkHailUI.spellKind = SpellKind.DarkHail;
+      //darkHailUI.spellName = "DarkHail";
+      //darkHailUI.spellStance = PlayerStance.Combat;
+      //darkHailUI.spellCost = 1;
+      //uiSpells.Add(darkHailUI);
     }
 
     private void CreateChain()
